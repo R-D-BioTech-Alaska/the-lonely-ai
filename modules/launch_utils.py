86a8b8b147818402eab1809b7aaa245a37032380
Brenton Carter <99493283+Inserian@users.noreply.github.com>
2023-06-23 08:21:53 -0800

    Update launch_utils.py

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -228,7 +228,7 @@ def prepare_environment():
     clip_package = os.environ.get('CLIP_PACKAGE', "https://github.com/openai/CLIP/archive/d50d76daa670286dd6cacf3bcd80b5e4823fc8e1.zip")
     openclip_package = os.environ.get('OPENCLIP_PACKAGE', "https://github.com/mlfoundations/open_clip/archive/bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b.zip")
 
-    the_lonely_ai_repo = os.environ.get('THE_LONELY_AI_REPO', "https://github.com/R-D-BioTech-Alaska/the-lonely-ai.git")
+    the_lonely_ai_repo = os.environ.get('THE_LONELY_AI_REPO', "https://github.com/R-D-BioTech-Alaska/the-lonely-ai/tree/main/lonely-ai")
     taming_transformers_repo = os.environ.get('TAMING_TRANSFORMERS_REPO', "https://github.com/CompVis/taming-transformers.git")
     k_diffusion_repo = os.environ.get('K_DIFFUSION_REPO', 'https://github.com/crowsonkb/k-diffusion.git')
     codeformer_repo = os.environ.get('CODEFORMER_REPO', 'https://github.com/sczhou/CodeFormer.git')
